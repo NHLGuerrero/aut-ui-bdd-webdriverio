8fbe7e8d9b44a25771e50f6c52a6529de70bc3e8
Nahuel Guerrero <nahuelferroguerrero@gmail.com>
2022-08-11 00:37:09 -0300

    armado de estructura de flujo de compras se crearon scenarios del flujo de compras se crearon los steps para los scenarios

---
+++ portafolio/step-definitions/compras.steps.ts
@@ -0,0 +1,18 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+
+
+When (/^Ordeno los productos con el (.*)$/, async (criterion:string) => {
+    
+});
+
+Then (/^Se muestra el titulo \"(.*?)\" en la pantalla de compras$/, async (title:string) => {
+
+});
+
+Then (/^Visualizo la pantalla de compras$/, async () => {
+
+});
+
+When (/^Completo los campos con (.+), (.+) y (.+)$/, async (firtsname:string, lastname:string, postalcode:number) => {
+    
+});
