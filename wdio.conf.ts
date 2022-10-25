99d3176cff6cce94472db0db8badc99e2b70ce84
Nahuel Guerrero <nahuelferroguerrero@gmail.com>
2022-10-25 20:04:12 -0300

    feat: github actions
    actualizacion de version de chrome
    
    actualizacion de typescript
    
    actualizacion ts-node
    
    actualizacion github actions
    
    actualizacion github actions
    
    actualizacion github actions
    
    actualizacion github actions
    
    actualizacion wdio.conf
    
    actualizacion de github actions
    
    actualizacion de github actions
    
    actualizacion de wdio.conf

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -76,20 +76,18 @@ export const config: WebdriverIO.Config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+     capabilities: [
+    {
+      maxInstances: 5,
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      // We need to extends some Chrome flags in order to tell Chrome to run headless
+      'goog:chromeOptions': {
+        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    ],
+
     //
     // ===================
     // Test Configurations
